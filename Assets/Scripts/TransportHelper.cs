4f9b5bd2974f32abd4c09ee111817905a8b592cc
jackhart591 <endersky79@gmail.com>
2023-03-02 12:54:48 -0800

    Integrated new server

---
+++ Assets/Scripts/TransportHelper.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Unity.Collections;
+using Unity.Networking.Transport;
+
+public static class TransportHelper {
+    public static void SendString(NetworkDriver driver, NetworkPipeline pipeline, NetworkConnection connection, string str) {
+
+        driver.BeginSend(pipeline, connection, out var writer);
+
+        // Creates a transportable string
+        FixedString4096Bytes formmatedStr = new FixedString4096Bytes(str);
+        writer.WriteFixedString4096(formmatedStr);
+
+        driver.EndSend(writer);
+
+        Debug.Log("Sent");
+    }
+
+    public static string ReceiveString(DataStreamReader stream) {
+        FixedString4096Bytes str = stream.ReadFixedString4096();
+		return str.ToString();
+    }
+}
--- Assets/Scripts/TreeDetection.cs
+++ Assets/Scripts/TreeDetection.cs
@@ -15,65 +15,6 @@ public class TreeDetection : MonoBehaviour {
         treesToRemove = new Dictionary<int, InputTree>();
     }
 
-    public async void FixedUpdate() {
-
-        List<InputTree>[] newData = CreateData();
-
-        await WebSocket.UpdateTrees(
-            new List<InputTree>(newData[0]),
-            new List<InputTree>(newData[1]),
-            new List<InputTree>(newData[2])
-        );
-    }
-
-    private List<InputTree>[] CreateData() {
-        Dictionary<int, InputTree> temp = new Dictionary<int, InputTree>();
-        Dictionary<int, List<object>> newPrevTrees = new Dictionary<int, List<object>>();
-        List<InputTree> removeTrees = new List<InputTree>();
-        createTrees = new List<InputTree>();
-
-        foreach (KeyValuePair<int, InputTree> tree in Trees) {
-            if (tree.Value.boundingBox.Center == Vector3.zero || tree.Value.boundingBox.Width == 0 || tree.Value.boundingBox.Height == 0) {
-                if (previousTrees.ContainsKey(tree.Key))
-                    removeTrees.Add(tree.Value);
-
-                continue; // if the tree is not being rendered
-            }
-
-            if (previousTrees.ContainsKey(tree.Key)) {
-                if (InputTree.isEqual(tree.Value, previousTrees[tree.Key])) {
-                    continue; // if there's nothing to update
-                }
-            }
-
-            // if trees don't match what it was last time, prepare to send
-            if (previousTrees.ContainsKey(tree.Key)) {
-                if (tree.Value.boundingBox.Center != (Vector3)previousTrees[tree.Key][2]) {
-                    temp.Add(tree.Key, tree.Value);
-                }
-            } else { createTrees.Add(tree.Value); } // if new tree
-
-            newPrevTrees.Add(tree.Key, new List<object> {
-                tree.Value.Age,
-                tree.Value.Species,
-                tree.Value.boundingBox.Center,
-                tree.Value.boundingBox.Width,
-                tree.Value.boundingBox.Height
-            });
-        }
-
-        foreach (KeyValuePair<int, InputTree> tree in treesToRemove) {
-            if (previousTrees.ContainsKey(tree.Key) && !removeTrees.Contains(tree.Value)) {
-                removeTrees.Add(tree.Value);
-            }
-        }
-
-        treesToRemove.Clear();
-        previousTrees = newPrevTrees;
-
-        return new List<InputTree>[3]{ createTrees, new List<InputTree>(temp.Values), removeTrees };
-    }
-
     public static void AddTree(int key, InputTree tree) {
         if (Trees.ContainsKey(key)) {
             Trees[key] = tree;
